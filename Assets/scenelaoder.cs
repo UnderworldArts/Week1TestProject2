44bd364653e598f5876882b5faa5e580b7d510bb
Psylones <map60944@gmail.com>
2026-07-13 15:03:27 +1200

    Update scenelaoder.cs

--- Assets/scenelaoder.cs
+++ Assets/scenelaoder.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class scenelaoder : MonoBehaviour
 {
+    private object sceneManager;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SceneManager.LoadScene("circle", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Character", LoadSceneMode.Additive);
+        SceneManager.LoadScene("UII", LoadSceneMode.Additive);
     }
 
     // Update is called once per frame
